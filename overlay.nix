186d76230ca3510cc713323d2599ee8b857d0571
asmx2 <asmx2@noreply.codeberg.org>
2026-09-02 13:16:30 +0300

    Drop VirtIO Venus

--- overlay.nix
+++ overlay.nix
@@ -3,12 +3,16 @@ final: prev:
 let
   gitVersion = "0-unstable-${builtins.substring 0 8 (mesa-src.rev or "0000000000000000")}";
   patchFn = pkgs: (import ./patches { inherit (pkgs) fetchpatch; });
-  buildGit = mesaPkg: mesaPkg.overrideAttrs (old: {
-    pname = "mesa-git";
-    version = gitVersion;
-    src = mesa-src;
-    patches = (old.patches or [ ]) ++ patchFn final;
-  });
+  buildGit = mesaPkg:
+    (mesaPkg.override {
+      # drop virtio (don't use this as a guest)
+      vulkanDrivers = builtins.filter (d: d != "virtio") mesaPkg.vulkanDrivers;
+    }).overrideAttrs (old: {
+      pname = "mesa-git";
+      version = gitVersion;
+      src = mesa-src;
+      patches = (old.patches or [ ]) ++ patchFn final;
+    });
 in
 {
   mesa_git = (buildGit prev.mesa);
