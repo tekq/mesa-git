b8a071f3643bc318419f0d12c0e0f8e68a1c366a
asmx2 <asmx2@noreply.codeberg.org>
2026-09-02 15:34:45 +0300

    Add workflow

--- flake.nix
+++ flake.nix
@@ -9,18 +9,22 @@
     };
   };
 
-  outputs = { self, nixpkgs, mesa-src }: {
-    overlays.default = import ./overlay.nix { inherit mesa-src; };
+  outputs = {
+    self,
+    nixpkgs,
+    mesa-src,
+  }: {
+    overlays.default = import ./overlay.nix {inherit mesa-src;};
 
-    nixosModules.default = { ... }: {
-      nixpkgs.overlays = [ self.overlays.default ];
+    nixosModules.default = {...}: {
+      nixpkgs.overlays = [self.overlays.default];
     };
 
-    packages.x86_64-linux =
-      let pkgs = import nixpkgs {
+    packages.x86_64-linux = let
+      pkgs = import nixpkgs {
         system = "x86_64-linux";
-        overlays = [ self.overlays.default ];
+        overlays = [self.overlays.default];
       };
-      in { inherit (pkgs) mesa_git mesa32_git; };
+    in {inherit (pkgs) mesa_git mesa32_git;};
   };
 }
--- overlay.nix
+++ overlay.nix
@@ -1,8 +1,6 @@
-{ mesa-src }:
-final: prev:
-let
+{mesa-src}: final: prev: let
   gitVersion = "0-unstable-${builtins.substring 0 8 (mesa-src.rev or "0000000000000000")}";
-  patchFn = pkgs: (import ./patches { inherit (pkgs) fetchpatch; });
+  patchFn = pkgs: (import ./patches {inherit (pkgs) fetchpatch;});
   buildGit = mesaPkg:
     (mesaPkg.override {
       # drop virtio (don't use this as a guest)
@@ -11,10 +9,9 @@ let
       pname = "mesa-git";
       version = gitVersion;
       src = mesa-src;
-      patches = (old.patches or [ ]) ++ patchFn final;
+      patches = (old.patches or []) ++ patchFn final;
     });
-in
-{
-  mesa_git = (buildGit prev.mesa);
-  mesa32_git = (buildGit prev.pkgsi686Linux.mesa);
+in {
+  mesa_git = buildGit prev.mesa;
+  mesa32_git = buildGit prev.pkgsi686Linux.mesa;
 }
